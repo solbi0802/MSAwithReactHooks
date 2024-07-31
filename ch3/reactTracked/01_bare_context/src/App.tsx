b057f723fabcd642639ffb8c38939edd7c6f4914
unknown <sbshin.dev@gmail.com>
2024-07-31 21:57:43 +0900

    불필요한 코드 제거

--- ch3/reactTracked/01_bare_context/src/App.tsx
+++ ch3/reactTracked/01_bare_context/src/App.tsx
@@ -1,10 +1,4 @@
-import {
-  Children,
-  ReactNode,
-  createContext,
-  useContext,
-  useState,
-} from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
 const useValue = () => useState({ count: 0, text: "hello" });
 
